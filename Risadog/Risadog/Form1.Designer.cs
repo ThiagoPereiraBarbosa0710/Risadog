2fec55d6a34d570f558f4283516a1c19dd4c8319
ThiagoPereiraBarbosa0710 <65790868+ThiagoPereiraBarbosa0710@users.noreply.github.com>
2024-05-17 03:02:03 -0300

    Add files via upload

---
+++ Risadog/Risadog/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Risadog
+{
+    partial class F_Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_User = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_Senha = new System.Windows.Forms.TextBox();
+            this.lbl_Senha = new System.Windows.Forms.Label();
+            this.btn_Entrar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_User
+            // 
+            this.lbl_User.AutoSize = true;
+            this.lbl_User.Location = new System.Drawing.Point(300, 134);
+            this.lbl_User.Name = "lbl_User";
+            this.lbl_User.Size = new System.Drawing.Size(46, 13);
+            this.lbl_User.TabIndex = 0;
+            this.lbl_User.Text = "Usuário:";
+            // 
+            // txt_User
+            // 
+            this.txt_User.Location = new System.Drawing.Point(303, 150);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(100, 20);
+            this.txt_User.TabIndex = 1;
+            // 
+            // txt_Senha
+            // 
+            this.txt_Senha.Location = new System.Drawing.Point(303, 189);
+            this.txt_Senha.Name = "txt_Senha";
+            this.txt_Senha.Size = new System.Drawing.Size(100, 20);
+            this.txt_Senha.TabIndex = 3;
+            // 
+            // lbl_Senha
+            // 
+            this.lbl_Senha.AutoSize = true;
+            this.lbl_Senha.Location = new System.Drawing.Point(300, 173);
+            this.lbl_Senha.Name = "lbl_Senha";
+            this.lbl_Senha.Size = new System.Drawing.Size(41, 13);
+            this.lbl_Senha.TabIndex = 2;
+            this.lbl_Senha.Text = "Senha:";
+            // 
+            // btn_Entrar
+            // 
+            this.btn_Entrar.Location = new System.Drawing.Point(276, 215);
+            this.btn_Entrar.Name = "btn_Entrar";
+            this.btn_Entrar.Size = new System.Drawing.Size(70, 23);
+            this.btn_Entrar.TabIndex = 4;
+            this.btn_Entrar.Text = "Entrar";
+            this.btn_Entrar.UseVisualStyleBackColor = true;
+            this.btn_Entrar.Click += new System.EventHandler(this.btn_Entrar_Click);
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(352, 215);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(70, 23);
+            this.btn_Cancelar.TabIndex = 5;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // F_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.btn_Entrar);
+            this.Controls.Add(this.txt_Senha);
+            this.Controls.Add(this.lbl_Senha);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.lbl_User);
+            this.Name = "F_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Risadog - Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_User;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_Senha;
+        private System.Windows.Forms.Label lbl_Senha;
+        private System.Windows.Forms.Button btn_Entrar;
+        private System.Windows.Forms.Button btn_Cancelar;
+    }
+}
+
